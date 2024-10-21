26989ea7c5e1ec0a368a66f784fccc2b896ddd57
APSALJAMAL <jamalapsal@gmail.com>
2024-10-21 14:55:29 +0530

    10

--- backend/server.js
+++ backend/server.js
@@ -42,10 +42,10 @@ app.use('/api/transactions', transactionRoutes);
 
 //app.use('/api/payment',paymentRoute);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'frontend', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
